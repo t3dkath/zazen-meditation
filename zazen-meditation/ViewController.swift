2aabc6c7e3411f01afbcda80e6c34c50725028b1
Kath Faulkner <kath@flipsidegroup.com>
2016-01-02 15:46:44 +0000

    Initial Commit

---
+++ zazen-meditation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  zazen-meditation
+//
+//  Created by Kath Faulkner on 02/01/2016.
+//  Copyright © 2016 T3D. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
